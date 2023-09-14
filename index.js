9149127ea84c706a83890a3b53ca17f7f99abbc0
rinaRenno <danalapa9@gmail.com>
2023-09-14 14:10:16 +0300

    step 2

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ export default function solution(content) {
     const newLog = log.split('|')
     return newLog
   })
+  
   const newMash = mash.map( (logic) => {
     const newLogic = logic.map( (cc) => {
       return cc.trim()
     })
     return newLogic.slice(1, newLogic.length - 1)
   } )
-  console.log(newMash); // Шаг 2
+  const plants = []
+  newMash.map( (pl) => plants.push(pl[0]) ).sort()
+  const newPlants = String(plants.sort().map( (npl) => {
+      if (npl[0] === npl[0].toUpperCase()) {
+        return ' ' + npl 
+      } else {
+        return ' ' + npl[0].toUpperCase() + npl.substring(1)
+      }
+    })).trim()
+  console.log(`Plants: ${newPlants}`); // Шаг 2
 };
