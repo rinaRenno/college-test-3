7214ede767d94c8183893464dfa6ea9b3b356acb
rinaRenno <danalapa9@gmail.com>
2023-09-14 11:34:15 +0300

    step 1.5

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 export default function solution(content) {
-  // BEGIN
-  console.log(content)
-  // END
-}
+  const data = content.split('\r\n')
+  const newData = data.slice(2, data.length)
+  console.log(`Count: ${newData.length}`) // Шаг 1
+
+  const mash = newData.map( (log) => {
+    const newLog = log.split('|')
+    return newLog
+  })
+  const newMash = mash.map( (logic) => {
+    const newLogic = logic.map( (cc) => {
+      return cc.trim()
+    })
+    return newLogic
+  } )
+  console.log(newMash);
+};
